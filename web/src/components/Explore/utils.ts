0565f7a6b269013082fedac884f31bd52e8b9542
Adam Jachymek <jachymek@outlook.com>
2023-02-18 14:03:21 +0100

    fix explore crash

--- web/src/components/Explore/utils.ts
+++ web/src/components/Explore/utils.ts
@@ -7,13 +7,13 @@ export const calculateFirstVisibleTile = (user: User) => {
   const tileX =
     user.x - 3 < 0
       ? 0
-      : user.x + 3 > NUMBER_OF_TILES_IN_AXIS
+      : user.x + 3 >= NUMBER_OF_TILES_IN_AXIS
       ? NUMBER_OF_TILES_IN_AXIS - NUMBER_OF_VISIBLE_TILES
       : user.x - 3;
   const tileY =
     user.y - 3 < 0
       ? 0
-      : user.y + 3 > NUMBER_OF_TILES_IN_AXIS
+      : user.y + 3 >= NUMBER_OF_TILES_IN_AXIS
       ? NUMBER_OF_TILES_IN_AXIS - NUMBER_OF_VISIBLE_TILES
       : user.y - 3;
   const tileIndex = tileY * 20 + tileX;
